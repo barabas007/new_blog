de7434e4b4aac57ab778d77ae580ef5d71be4eac
igor shilov <shop.ru.shop@mail.ru>
2020-11-08 13:52:42 +0300

    2_39

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+  def index 
+    @articles = Article.all
+  end
   def show 
     @article = Article.find(params[:id])
   end
@@ -7,12 +10,24 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    if @article.valid? 
-      @article.save
+    if @article.save
       redirect_to @article
     else 
       render action: 'new'
     end
+  end
+
+  def edit
+    @article = Article.find(params[:id]) 
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else 
+      render action: 'edit'
+    end 
   end
 
   private
